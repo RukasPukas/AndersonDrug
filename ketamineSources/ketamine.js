fac570d9b1b499a2f13f1f261849f918a20fa269
Luke Tyon <lukastyon@gmail.com>
2025-05-18 10:10:49 -0500

    Add Labetalol files

--- ketamineSources/ketamine.js
+++ ketamineSources/ketamine.js
@@ -22,25 +22,23 @@ $(document).ready(function () {
       console.log("Converted weight to kg:", weightInput);
     }
 
-   const protocolSelected = $('input[name="protocol"]:checked').val();
-   console.log("Selected protocol:", protocolSelected);
+    const protocolSelected = $('input[name="protocol"]:checked').val();
+    console.log("Selected protocol:", protocolSelected);
 
-
-   if(protocolSelected === "Behavioral"){
-    
-    let agitatedDose = weightInput * 4;
-    console.log("Agitated dose:", agitatedDose);
-    if(agitatedDose > 400){
+    if (protocolSelected === "Behavioral") {
+      let agitatedDose = weightInput * 4;
+      console.log("Agitated dose:", agitatedDose);
+      if (agitatedDose > 400) {
         agitatedDose = 400;
         console.log("Agitated dose capped at 400:", agitatedDose);
-    }
+      }
 
-    agitatedVolume = agitatedDose / 50;
-    console.log("Agitated volume:", agitatedVolume);
+      agitatedVolume = agitatedDose / 50;
+      console.log("Agitated volume:", agitatedVolume);
 
-    $("#result")
-      .html(
-        `
+      $("#result")
+        .html(
+          `
         <h2>Agitated or Violent Patient/Behavioral Emergencies:</h2>
         <p> <strong>Suggested Dosage:</strong> ${agitatedDose.toFixed(2)}mg
         <br>
@@ -54,33 +52,34 @@ $(document).ready(function () {
         <br>
         <br>
         <small><i>Note: Always ensure amounts are accurate prior to administration. 
-        Consult Anderson Protocols or Medical Control if uncertainty exists.</small></i>
+        Consult Anderson Protocols or Medical Control if uncertainty exists.
+        </small></i>
          </p>
             
         `
-      )
-      .fadeIn(200);
+        )
+        .fadeIn(200);
     }
-if(protocolSelected === "Pain"){
-    let painIN = weightInput * .5;
-    console.log("Pain IN dose:", painIN);
-    if(painIN > 20){
+    if (protocolSelected === "Pain") {
+      let painIN = weightInput * 0.5;
+      console.log("Pain IN dose:", painIN);
+      if (painIN > 20) {
         painIN = 20;
         console.log("Pain IN dose capped at 20:", painIN);
-    }
-    let painIV = weightInput * .2;
-    console.log("Pain IV dose:", painIV);
-    if(painIV > 20){
+      }
+      let painIV = weightInput * 0.2;
+      console.log("Pain IV dose:", painIV);
+      if (painIV > 20) {
         painIV = 20;
         console.log("Pain IV dose capped at 20:", painIV);
-    }
-    let painVolumeIV = painIV / 50;
-    console.log("Pain IV volume:", painVolumeIV);
-    let painVolumeIN = painIN / 50;
-    console.log("Pain IN volume:", painVolumeIN);
-    $("#result")
-      .html(
-        `
+      }
+      let painVolumeIV = painIV / 50;
+      console.log("Pain IV volume:", painVolumeIV);
+      let painVolumeIN = painIN / 50;
+      console.log("Pain IN volume:", painVolumeIN);
+      $("#result")
+        .html(
+          `
         <h2>Acute Pain Management:</h2>
         <p><strong><u>Intravenous</u>
         <br>Suggested Dosage:</strong> ${painIV.toFixed(2)}mg IV
@@ -108,30 +107,37 @@ if(protocolSelected === "Pain"){
          </p>
             
         `
-      )
-      .fadeIn(200);
+        )
+        .fadeIn(200);
     }
 
-if(protocolSelected === "Intubation"){
-assistedIntubationDose = weightInput * 2;
-    console.log("Assisted intubation dose:", assistedIntubationDose);
-    if(assistedIntubationDose > 200){
+    if (protocolSelected === "Intubation") {
+      assistedIntubationDose = weightInput * 2;
+      console.log("Assisted intubation dose:", assistedIntubationDose);
+      if (assistedIntubationDose > 200) {
         assistedIntubationDose = 200;
-        console.log("Assisted intubation dose capped at 200:", assistedIntubationDose);
-    }
-    let assistedIntubationVolume = assistedIntubationDose / 50;
-    console.log("Assisted intubation volume:", assistedIntubationVolume);
+        console.log(
+          "Assisted intubation dose capped at 200:",
+          assistedIntubationDose
+        );
+      }
+      let assistedIntubationVolume = assistedIntubationDose / 50;
+      console.log("Assisted intubation volume:", assistedIntubationVolume);
 
-    $("#result")
-      .html(
-        `
+      $("#result")
+        .html(
+          `
         <h2>Assisted Intubation:</h2>
-        <p><strong>Suggested Dosage IV/IO:</strong> ${assistedIntubationDose.toFixed(2)}mg
+        <p><strong>Suggested Dosage IV/IO:</strong> ${assistedIntubationDose.toFixed(
+          2
+        )}mg
         <br>
         <small>(Method: 2mg x ${weightInput.toFixed(2)}kg MAX: 200mg)</small>
         <br>
         <br>
-        <strong>Suggested Volume:</strong> ${assistedIntubationVolume.toFixed(2)}ml
+        <strong>Suggested Volume:</strong> ${assistedIntubationVolume.toFixed(
+          2
+        )}ml
         <br><small>(For Concentrations of 50mg per ml)
         <br>
         <small><i>Note: Always ensure amounts are accurate prior to administration. 
@@ -139,25 +145,29 @@ assistedIntubationDose = weightInput * 2;
          </p>
             
         `
-      )
-      .fadeIn(200);
+        )
+        .fadeIn(200);
     }
-    if(protocolSelected === "postIntubation"){
-        let postIntubationDose = weightInput * 1;
-        console.log("Post Intubation dose:", postIntubationDose);
-        if(postIntubationDose > 200){
-            postIntubationDose = 200;
-            console.log("Post intubation dose capped at 200:", postIntubationDose);
-        }
-        let sedationVolume = postIntubationDose / 50;
-        console.log("Post intubation volume:", postIntubationDose);
-        $("#result")
-          .html(
-            `
+    if (protocolSelected === "postIntubation") {
+      let postIntubationDose = weightInput * 1;
+      console.log("Post Intubation dose:", postIntubationDose);
+      if (postIntubationDose > 200) {
+        postIntubationDose = 200;
+        console.log("Post intubation dose capped at 200:", postIntubationDose);
+      }
+      let sedationVolume = postIntubationDose / 50;
+      console.log("Post intubation volume:", postIntubationDose);
+      $("#result")
+        .html(
+          `
             <h2>Post Intubation Sedation:</h2>
-            <p><strong>Suggested Dosage IV/IO:</strong> ${postIntubationDose.toFixed(2)}mg
+            <p><strong>Suggested Dosage IV/IO:</strong> ${postIntubationDose.toFixed(
+              2
+            )}mg
             <br>
-            <small>(Method: 1mg x ${weightInput.toFixed(2)}kg MAX: 200mg)</small>
+            <small>(Method: 1mg x ${weightInput.toFixed(
+              2
+            )}kg MAX: 200mg)</small>
             <br>
             <br>
             <strong>Suggested Volume:</strong> ${sedationVolume.toFixed(2)}ml
@@ -173,11 +183,8 @@ assistedIntubationDose = weightInput * 2;
              </p>
                 
             `
-          )
-          .fadeIn(200);
-        }
-
-
+        )
+        .fadeIn(200);
+    }
   });
-  
 });
