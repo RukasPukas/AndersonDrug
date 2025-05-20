05ed5343d8a873acba07fff073cd12d2a12bb3e7
Luke Tyon <lukastyon@gmail.com>
2025-05-19 21:09:18 -0500

    Add MagSulfate Files

--- magnesiumSources/magnesium.js
+++ magnesiumSources/magnesium.js
@@ -2,88 +2,27 @@ $(document).ready(function () {
   $("body").fadeIn(750);
 
   $("#calculateDosage").on("click", function () {
-    const initialInput = $("#weightInput").val();
-    console.log("Initial input:", initialInput);
-
-    let weightInput = parseFloat(initialInput);
-    console.log("Parsed weight input:", weightInput);
-
-    if (isNaN(weightInput) || weightInput <= 0) {
-      alert("Please enter a valid weight.");
-      return;
-    }
-
-    const unit = $('input[name="unit"]:checked').val();
-    console.log("Selected unit:", unit);
-
-    if (unit === "lbs") {
-      weightInput = weightInput / 2.20462;
-      weightInput = parseFloat(weightInput.toFixed(2));
-      console.log("Converted weight to kg:", weightInput);
-    }
-
     //Begin Adult Protocols
     const protocolSelected = $('input[name="protocol"]:checked').val();
     console.log("Selected protocol:", protocolSelected);
 
-    if (protocolSelected === "Tachycardia") {
+    if (protocolSelected === "respiratory") {
       console.log(protocolSelected);
 
-      let lInitial = weightInput * 1;
-      let hInitial = weightInput * 1.5;
-      let repeat = weightInput * 0.75;
-
-      if (lInitial > 1000) {
-        alert("Please enter a valid weight");
-        return;
-      }
-      if (hInitial > 1000) {
-        alert("Please enter a valid weight");
-        return;
-      }
-      if (repeat > 1000) {
-        alert("Please enter a valid weight");
-        return;
-      }
-
-      console.log("Dosage range is", lInitial, "-", hInitial, "mg/kg");
-      console.log("Repeat Dosage is: ", repeat);
+      //2 grams IV in 50 mL NS over 10-15 minutes
+      const respiratoryDose = 2;
 
       $("#result")
         .html(
           `
-        <h2><u>Adult</u><br> Tachycardia (with a Pulse) - Wide Complex; <br><br>
-        Cardiac Arrest - (Vfib / Pulseless V-Tach);
+        <h2><u>Adult</u><br> Bronchospasm / Asthma / COPD;
         </h2>
-        <p> <strong>Suggested <u>Initial</u> Dose Range:</strong> 
+        <p> <strong>Method:</strong> 
         <br>
         <br>
-        ${lInitial.toFixed(2)}mg - ${hInitial.toFixed(2)}mg IV/IO
-        <br>
-        <small>(Method: ${weightInput}kg x 1mg/kg -<u>OR</u>- ${weightInput}kg x 1.5mg/kg)</small>
-        <br>
-        <br>
-        <br>
-        <br>
-        <strong>Repeat Dosaging:</strong>
-        <br>
-        <br>
-        <u>IF NEEDED</u> may repeat the following dosage x2 in 3-5 minute increments:
-        <br>
-        <br>${repeat.toFixed(2)}mg IV/IO
-        <br>
-        <small>(Method: ${weightInput.toFixed(2)}kg x 0.75mg/kg)</small>
-        <br>
-        <br>
-        <br>
-        <br>
-        <strong>If Tachycardia Resolves With Bolus:</strong>
-        <br>
-        <br> Administer maintenance infusion at 2-4 mg/min.
-        <br>
-        <br>
-        <br>
-
+        Mix <strong>2 grams</strong> of Magnesium Sulfate into <strong>50ml</strong> of normal saline and infuse over <strong>10-15 minutes</strong>.
+          <br>
+          <br>
 
         <small><i>Note: Always ensure amounts are accurate prior to administration. 
         Consult Anderson Protocols or Medical Control if uncertainty exists.
@@ -94,20 +33,72 @@ $(document).ready(function () {
         )
         .fadeIn(200);
     }
-    if (protocolSelected === "Pain") {
+    if (protocolSelected === "eclampsia") {
       console.log(protocolSelected);
 
       $("#result")
         .html(
           `
-        <h2><u>Adult</u><br>Intraosseous Access-Responsive To Pain</h2>
-        <p> <strong>Suggested Dose:</strong> 
+        <h2><u>Adult</u><br>Eclampsia / Pre-Eclapmsia;</h2>
+        <p> 
+          <strong>Method:</strong>
         <br>
         <br>
-        <strong>40mg <br></strong>
-        <small><i>Through IO insertion site, over 120 seconds.</i></small>
+        Mix <strong>4 grams </strong> of Magnesium Sulfate in<strong> 50mL</strong> of normal saline and infuse over <strong>10-20 minutes for seizures</strong>.
         <br>
         <br>
+
+        <small><i>Note: Always ensure amounts are accurate prior to administration. 
+        Consult Anderson Protocols or Medical Control if uncertainty exists.
+        </small></i>
+         </p>
+            
+        `
+        )
+        .fadeIn(200);
+    }
+
+    if (protocolSelected === "torsadespulse") {
+      console.log(protocolSelected);
+
+      $("#result")
+        .html(
+          `
+        <h2><u>Adult</u><br>Tachycardia (with a Pulse)-Wide Complex (Torsades);</h2>
+        <p> 
+          <strong>Method:</strong>
+        <br>
+        <br>
+        <strong>2 grams</strong> <u>SLOW</u> IV/IO push (over 2-3 minutes).
+        <br>
+        <br>
+
+        <small><i>Note: Always ensure amounts are accurate prior to administration. 
+        Consult Anderson Protocols or Medical Control if uncertainty exists.
+        </small></i>
+         </p>
+            
+        `
+        )
+        .fadeIn(200);
+    }
+
+    if (protocolSelected === "torsadespulseless") {
+      console.log(protocolSelected);
+
+      $("#result")
+        .html(
+          `
+        <h2><u>Adult</u><br>Cardiac Arrest-(V-Fib/Pulseless V-Tach)(Torsades)
+        </h2>
+        <p> 
+          <strong>Method:</strong>
+        <br>
+        <br>
+        <strong>2 grams</strong> IV/IO push (over 1-2 minutes).
+        <br>
+        <br>
+
         <small><i>Note: Always ensure amounts are accurate prior to administration. 
         Consult Anderson Protocols or Medical Control if uncertainty exists.
         </small></i>
@@ -147,58 +138,245 @@ $(document).ready(function () {
     const protocolSelected = $('input[name="Pediprotocol"]:checked').val();
     console.log("Selected pediatric protocol:", protocolSelected);
 
-    if (protocolSelected === "Pain") {
+    if (protocolSelected === "Respiratory") {
+      console.log("Pediatric: ", protocolSelected);
+
+      let doseInMg = 50 * parseFloat(pediWeightInput);
+      let respiratoryDose;
+
+      if (doseInMg >= 2000) {
+        respiratoryDose = "2 grams";
+      } else {
+        respiratoryDose = doseInMg.toFixed(2) + " mg";
+      }
+
+      let respiratoryVolume = doseInMg / 500;
+
       $("#resultPedi")
         .html(
           `
-        <h2><u>Pediatric</u><br>Intraosseous Access-Responsive To Pain</h2>
-        <p> <strong>Suggested Dose:</strong> 
-        <br>
-        <br>
-        <strong>40mg <br></strong>
-        <small><i>Through IO insertion site, over 120 seconds.</i></small>
-        <br>
-        <br>
-        <small><i>Note: Always ensure amounts are accurate prior to administration. 
-        Consult Anderson Protocols or Medical Control if uncertainty exists.
-        </small></i>
-         </p>
-            
-        `
+      <h2><u>Pediatric</u><br>Respiratory Distress-Lower Airway;<br><br><i><u>MEDICAL CONTROL MUST AUTHORIZE</u></i></h2>
+      <p> <strong>Suggested Dose:</strong> 
+      <br>
+      <br>
+      <strong>${respiratoryDose} in 50ml of normal saline, over 10-15 minutes.<br></strong>
+      <small>(Method: 50mg/kg x ${pediWeightInput}kg = ${doseInMg}mg MAX of 2000mg)</small>
+      <br>
+      <br>
+      <strong>Suggested Volume:</strong>
+      <br>
+      <u>(For Concentrations of <strong>500mg per 1ml</strong>)</u>
+      <br>
+      <br>
+      <strong>${respiratoryVolume.toFixed(
+        2
+      )} ml</strong> of Magnesium Sulfate infused into <strong>50ml</strong> of normal saline.
+       <br>
+       <br>
+      <small><i>Note: Always ensure amounts are accurate prior to administration. 
+      Consult Anderson Protocols or Medical Control if uncertainty exists.
+      </small></i>
+       </p>
+      `
         )
         .fadeIn(200);
     }
 
-    if (protocolSelected === "Tachycardia") {
-      $("#resultPedi")
-        .html(
-          `
-        <h2><u>
-        Pediatric</u><br> Tachycardia (with a Pulse) - Wide Complex;
-        <br>
-        <br>
-        Cardiac Arrest - (Vfib / Pulseless V-Tach);
-        </h2>
-        <p> <strong>Suggested Dose:</strong> 
-        <br>
-        <br>
-        <strong>${pediWeightInput.toFixed(0)}mg IV/IO</strong>
-        <br>
-        <small>(Method: ${pediWeightInput}kgs x 1mg/kg)</small>
-        <br><br>
-        <strong>If Tachycardia Resolves With Bolus:</strong>
-        <br><br>
-        Maintenance infusion at 20-50 mcg/kg/min.
-        <br>
-        <br>
-        <small><i>Note: Always ensure amounts are accurate prior to administration. 
-        Consult Anderson Protocols or Medical Control if uncertainty exists.
-        </small></i>
-         </p>
-            
-        `
-        )
-        .fadeIn(200);
+    if (protocolSelected === "PediPulseTorsades") {
+      console.log("Pediatric: ", protocolSelected);
+
+      let torsadesLDose = 25 * parseFloat(pediWeightInput);
+      let torsadesHDose = 50 * parseFloat(pediWeightInput);
+
+      if (torsadesLDose >= 2000 && torsadesHDose >= 2000) {
+        let torsadesVolume = torsadesLDose / 500;
+        $("#resultPedi")
+          .html(
+            `
+      <h2><u>Pediatric</u><br>Tachycardia (with a Pulse)-Wide Complex (Torsades)</h2>
+      <p> <strong>Suggested Dose:</strong> 
+      <br>
+      <br>
+      <strong>${torsadesLDose}mg in 50ml of normal saline, over 10 minutes.<br></strong>
+      <small>(Method: 25-50mg/kg x ${pediWeightInput}kg = ${torsadesLDose}mg MAX of 2000mg)</small>
+      <br>
+      <br>
+      <strong>Suggested Volume:</strong>
+      <br>
+      <u>(For Concentrations of <strong>500mg per 1ml</strong>)</u>
+      <br>
+      <br>
+      <strong>${torsadesVolume.toFixed(
+        2
+      )} ml</strong> of Magnesium Sulfate infused into <strong>50ml</strong> of normal saline.
+       <br>
+       <br>
+      <small><i>Note: Always ensure amounts are accurate prior to administration. 
+      Consult Anderson Protocols or Medical Control if uncertainty exists.
+      </small></i>
+       </p>
+      `
+          )
+          .fadeIn(200);
+      } else if (torsadesLDose < 2000 && torsadesHDose >= 2000) {
+        torsadesHDose = 2000;
+        let torsadesLVolume = torsadesLDose / 500;
+        let torsadesHVolume = 4;
+        $("#resultPedi")
+          .html(
+            `
+      <h2><u>Pediatric</u><br>Tachycardia (with a Pulse)-Wide Complex (Torsades)</h2>
+      <p> <strong>Suggested Dose Range:</strong> 
+      <br>
+      <br>
+      <strong>${torsadesLDose}mg - ${torsadesHDose}mg in 50ml of normal saline, over 10 minutes.<br></strong>
+      <small>(Method: 25-50mg/kg x ${pediWeightInput}kg = ${torsadesLDose}mg -${torsadesHDose}mg MAX of 2000mg)</small>
+      <br>
+      <br>
+      <strong>Suggested Volume Range:</strong>
+      <br>
+      <u>(For Concentrations of <strong>500mg per 1ml</strong>)</u>
+      <br>
+      <br>
+      <strong>${torsadesLVolume.toFixed(
+        2
+      )} ml - ${torsadesHVolume} ml</strong> of Magnesium Sulfate infused into <strong>50ml</strong> of normal saline.
+       <br>
+       <br>
+      <small><i>Note: Always ensure amounts are accurate prior to administration. 
+      Consult Anderson Protocols or Medical Control if uncertainty exists.
+      </small></i>
+       </p>
+      `
+          )
+          .fadeIn(200);
+      } else if (torsadesLDose < 2000 && torsadesHDose < 2000) {
+        let torsadesLVolume = torsadesLDose / 500;
+        let torsadesHVolume = torsadesHDose / 500;
+        $("#resultPedi")
+          .html(
+            `
+      <h2><u>Pediatric</u><br>Tachycardia (with a Pulse)-Wide Complex (Torsades)</h2>
+      <p> <strong>Suggested Dose Range:</strong> 
+      <br>
+      <br>
+      <strong>${torsadesLDose}mg - ${torsadesHDose}mg in 50ml of normal saline, over 10 minutes.<br></strong>
+      <small>(Method: 25-50mg/kg x ${pediWeightInput}kg = ${torsadesLDose}mg -${torsadesHDose}mg MAX of 2000mg)</small>
+      <br>
+      <br>
+      <strong>Suggested Volume Range:</strong>
+      <br>
+      <u>(For Concentrations of <strong>500mg per 1ml</strong>)</u>
+      <br>
+      <br>
+      <strong>${torsadesLVolume.toFixed(
+        2
+      )} ml - ${torsadesHVolume} ml</strong> of Magnesium Sulfate infused into <strong>50ml</strong> of normal saline.
+       <br>
+       <br>
+      <small><i>Note: Always ensure amounts are accurate prior to administration. 
+      Consult Anderson Protocols or Medical Control if uncertainty exists.
+      </small></i>
+       </p>
+      `
+          )
+          .fadeIn(200);
+      }
+    }
+    if (protocolSelected === "PediPulselessTorsades") {
+      console.log("Pediatric: ", protocolSelected);
+
+      let torsadesLDose = 25 * parseFloat(pediWeightInput);
+      let torsadesHDose = 50 * parseFloat(pediWeightInput);
+
+      if (torsadesLDose >= 2000 && torsadesHDose >= 2000) {
+        let torsadesVolume = torsadesLDose / 500;
+        $("#resultPedi")
+          .html(
+            `
+      <h2><u>Pediatric</u><br>Tachycardia (with a Pulse)-Wide Complex (Torsades)</h2>
+      <p> <strong>Suggested Dose:</strong> 
+      <br>
+      <br>
+      <strong>${torsadesLDose}mg IV/IO over 1-2 minutes..<br></strong>
+      <small>(Method: 25-50mg/kg x ${pediWeightInput}kg = ${torsadesLDose}mg MAX of 2000mg)</small>
+      <br>
+      <br>
+      <strong>Suggested Volume:</strong>
+      <br>
+      <u>(For Concentrations of <strong>500mg per 1ml</strong>)</u>
+      <br>
+      <br>
+      <strong>${torsadesVolume.toFixed(2)} ml</strong> 
+       <br>
+       <br>
+      <small><i>Note: Always ensure amounts are accurate prior to administration. 
+      Consult Anderson Protocols or Medical Control if uncertainty exists.
+      </small></i>
+       </p>
+      `
+          )
+          .fadeIn(200);
+      } else if (torsadesLDose < 2000 && torsadesHDose >= 2000) {
+        torsadesHDose = 2000;
+        let torsadesLVolume = torsadesLDose / 500;
+        let torsadesHVolume = 4;
+        $("#resultPedi")
+          .html(
+            `
+      <h2><u>Pediatric</u><br>Tachycardia (with a Pulse)-Wide Complex (Torsades)</h2>
+      <p> <strong>Suggested Dose Range:</strong> 
+      <br>
+      <br>
+      <strong>${torsadesLDose}mg - ${torsadesHDose}mg IV/IO over 1-2 minutes.<br></strong>
+      <small>(Method: 25-50mg/kg x ${pediWeightInput}kg = ${torsadesLDose}mg -${torsadesHDose}mg MAX of 2000mg)</small>
+      <br>
+      <br>
+      <strong>Suggested Volume Range:</strong>
+      <br>
+      <u>(For Concentrations of <strong>500mg per 1ml</strong>)</u>
+      <br>
+      <br>
+      <strong>${torsadesLVolume.toFixed(2)} ml - ${torsadesHVolume} ml</strong>.
+       <br>
+       <br>
+      <small><i>Note: Always ensure amounts are accurate prior to administration. 
+      Consult Anderson Protocols or Medical Control if uncertainty exists.
+      </small></i>
+       </p>
+      `
+          )
+          .fadeIn(200);
+      } else if (torsadesLDose < 2000 && torsadesHDose < 2000) {
+        let torsadesLVolume = torsadesLDose / 500;
+        let torsadesHVolume = torsadesHDose / 500;
+        $("#resultPedi")
+          .html(
+            `
+      <h2><u>Pediatric</u><br>Tachycardia (with a Pulse)-Wide Complex (Torsades)</h2>
+      <p> <strong>Suggested Dose Range:</strong> 
+      <br>
+      <br>
+      <strong>${torsadesLDose}mg - ${torsadesHDose}mg IV/IO over 1-2 minutes.<br></strong>
+      <small>(Method: 25-50mg/kg x ${pediWeightInput}kg = ${torsadesLDose}mg -${torsadesHDose}mg MAX of 2000mg)</small>
+      <br>
+      <br>
+      <strong>Suggested Volume Range:</strong>
+      <br>
+      <u>(For Concentrations of <strong>500mg per 1ml</strong>)</u>
+      <br>
+      <br>
+      <strong>${torsadesLVolume.toFixed(2)} ml - ${torsadesHVolume} ml</strong>.
+       <br>
+       <br>
+      <small><i>Note: Always ensure amounts are accurate prior to administration. 
+      Consult Anderson Protocols or Medical Control if uncertainty exists.
+      </small></i>
+       </p>
+      `
+          )
+          .fadeIn(200);
+      }
     }
   });
 });
