8acc5e173345566ee1149f6ed394fe16923da0c2
Luke Tyon <lukastyon@gmail.com>
2025-06-05 10:34:32 -0500

    Add dropdown spinner for medications along with CSS changes and updated JS

--- indexSources/indexjs.js
+++ indexSources/indexjs.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-  $("body").delay(1000).fadeIn(750); 
+  $("body").delay(1000).fadeIn(750);
+
+  $("#toggleDrugs").on("click", function () {
+    $("#drugList").slideToggle("slow");
+
+    const isOpen = $(this).text().includes("▲");
+    $(this).text(isOpen ? "▼ View Drug List" : "▲ Hide Drug List");
+  });
 });
