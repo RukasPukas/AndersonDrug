a808a8d560cea0133ff86eacd8d8f8bdf2d0f5a3
Luke Tyon <lukastyon@gmail.com>
2025-06-06 10:00:22 -0500

    Add legal disclaimer

--- indexSources/indexjs.js
+++ indexSources/indexjs.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
-  $("body").delay(1000).fadeIn(750);
+  $("body").css("overflow", "hidden");
+
+  $("#agree-checkbox").on("change", function () {
+    $("#accept-button").prop("disabled", !this.checked);
+  });
+
+  $("#accept-button").on("click", function () {
+    $("#disclaimer-modal").fadeOut(300, function () {
+      $("body").css("overflow", "auto").fadeIn(750);
+    });
+  });
 
   $("#toggleDrugs").on("click", function () {
     $("#drugList").slideToggle("slow");
