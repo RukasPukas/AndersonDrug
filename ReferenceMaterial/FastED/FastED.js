d6fda031dc1983a8404284d50d5a8fc0afb57da6
Lukibear <lukastyon@gmail.com>
2025-07-23 21:05:48 -0500

    Add initial stages of FAST-ED

--- ReferenceMaterial/FastED/FastED.js
+++ ReferenceMaterial/FastED/FastED.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
   $("body").fadeIn(750);
+
+  $(".score-cell").on("click", function () {
+    const $table = $(this).closest("table");
+    
+   
+    $table.find(".score-cell").removeClass("selected dimmed");
+
+   
+    $(this).addClass("selected");
+
+   
+    $table.find(".score-cell").not(this).addClass("dimmed");
+
+    let score = $(this).data("point");
+    console.log("Selected FAST-ED score:", score);
+  });
 });
