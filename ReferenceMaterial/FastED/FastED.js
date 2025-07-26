d5b3d2b15d73c9671430de3d3ee1b1269d8ad367
Luke Tyon <lukastyon@gmail.com>
2025-07-26 16:53:40 -0500

    near completion of Fast-ED

--- ReferenceMaterial/FastED/FastED.js
+++ ReferenceMaterial/FastED/FastED.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
   $("body").fadeIn(750);
+  if (localStorage.getItem("fastedSubmitted") === "true") {
+    const savedReport = localStorage.getItem("fastedReport");
+    $(".TablesWrapper").hide();
+    $(".FastEDHeader").hide();
+    $(".ScoreWrapper").show();
+    $("#Report").html(savedReport).show();
+  }
 
   $(".score-cell").on("click", function () {
     const $table = $(this).closest("table");
@@ -17,6 +24,13 @@ $(document).ready(function () {
     console.log("Selected FAST-ED score:", $(this).data("point"));
   });
 
+  $("#resetBtn").on("click", function () {
+    localStorage.removeItem("fastedReport");
+    localStorage.removeItem("fastedSubmitted");
+    localStorage.removeItem("fastedState");
+    location.reload();
+  });
+
   $("#calculateScore").on("click", function () {
     let lkwDate = $("#LKWD").val();
     let lkwTime = $("#LKWT").val();
@@ -27,6 +41,11 @@ $(document).ready(function () {
 
     if (!lkwDate || !lkwTime) {
       alert("Please enter both the Last Known Well date and time.");
+      return;
+    }
+
+    if (!SST || !SSD) {
+      alert("Please enter both the Symptom Onset date and time.");
       return;
     }
 
@@ -89,70 +108,115 @@ $(document).ready(function () {
     if (scoreTotal === 0) {
       reportHtml = `
         <h2>FAST-ED Score Report</h2>
-        <h3>Score: ${scoreTotal}<br><br>
+        <h3>Fast-ED Score: ${scoreTotal}<br><br>
         Proceed to appropriate SOG/SOP and transport accordingly.</h3>
       `;
     } else if (scoreTotal > 0) {
       reportHtml = `
         <h2>FAST-ED Score Report</h2>
-        <h3>Score: ${scoreTotal}<br><br>
-        Last Known Well: ${lkwTime} on ${lkwDate}<br><br> 
-        Symptom Onset: ${SST} on ${SSD}<br><br>
-        Time Elapsed Since Last Known Well: ${hours} hour${
+        <h3>Fast-ED Score: ${scoreTotal}<br><br>
+        </h3>
+        <h2> Time Breakdown</h2>
+        <h3>
+        Report Generated:</h3> <p>${now}</p>
+        <h3>Last Known Well: </h3><p>${lkwTime} on ${lkwDate}</p>
+        <h3>Symptom Onset: </h3><p>${SST} on ${SSD}</p>
+        <h3>Time Elapsed Since Last Known Well:</h3><p> ${hours} hour${
         hours !== 1 ? "s" : ""
       } 
-        and ${minutes} minute${minutes !== 1 ? "s" : ""}</h3>
+        and ${minutes} minute${minutes !== 1 ? "s" : ""}</p>
       `;
 
       if (timeStatus === "within4_5hrs") {
         reportHtml += `
-          <h3>Time since Last Known Well is within the 4.5-hour window with symptoms. <br><u>Call Code Stroke Alert</u></h3>
+          <p>Time since Last Known Well is within the 4.5-hour window with symptoms. 
+          <h2>Action</h2><p>Call Code Stroke Alert</p>
         `;
 
         if (scoreTotal >= 4) {
           reportHtml += `
-          <h3>Transport to a thrombectomy capable center if transport time does not add 15 or more minutes.</h3>
+          <p>Transport to a thrombectomy capable center if transport time does not add 15 or more minutes.</p>
         `;
         } else {
           reportHtml += `
-          <h3>Transport to nearest Acute Stroke Ready or Primary Stroke Center.</h3>
+          <p>Transport to nearest Acute Stroke Ready or Primary Stroke Center.</p>
         `;
         }
       } else if (timeStatus === "within24hrs") {
         reportHtml += `
-          <h3>Time since Last Known Well is within the 4.5-24 hour window with symptoms. <br><ul>Call Code Stroke Alert</ul></h3>
+          <p>Time since Last Known Well is within the 4.5-24 hour window with symptoms.<h2>Action</h2><p>Call Code Stroke Alert</p>
         `;
 
         if (scoreTotal >= 4) {
           reportHtml += `
-          <h3>Transport to nearest thrombectomy capable center.</h3>
+          <p>Transport to nearest thrombectomy capable center.</p>
         `;
         } else {
           reportHtml += `
-          <h3>Transport to nearest Acute Stroke Ready or Primary Stroke Center.</h3>
+          <p>Transport to nearest Acute Stroke Ready or Primary Stroke Center.</p>
         `;
         }
       } else {
         reportHtml += `
-          <h3>Time since Last Known Well is beyond the 24-hour window with symptoms.</h3>
-                    <h3>Transport to nearest Acute Stroke Ready or Primary Stroke Center.</h3>
+          <p>Time since Last Known Well is beyond the 24-hour window with symptoms.</p>
+          <h2>Action</h2>
+                    <p>Transport to nearest Acute Stroke Ready or Primary Stroke Center.</p>
         `;
       }
 
-      const reportedThinners =
-        $("#ThinnersID input[type=checkbox]:checked")
-          .map(function () {
-            return $(this).val();
-          })
-          .get()
-          .join(", ") || "None";
+      if ((YesCell = $("#YesCell").hasClass("selected"))) {
+        const reportedThinners =
+          $("#ThinnersID input[type=checkbox]:checked")
+            .map(function () {
+              return $(this).val();
+            })
+            .get()
+            .join(", ") || "None";
 
-      reportHtml += `
-  <h3>Reported Blood Thinners: ${reportedThinners}</h3>
+        reportHtml += `
+        <h2>Additional Information</h2>
+  <h3>Reported Blood Thinners:</h3><p> ${reportedThinners}</p>
 `;
+      } else if ($("#NoCell").hasClass("selected")) {
+        reportHtml += `
+        <h2>Additional Information</h2>
+        <h3>Blood Thinner Usage:</h3><p>No reported blood thinners.</p>
+      `;
+      } else {
+        reportHtml += `
+        <h2>Additional Information</h2>
+        <h3>Blood Thinner Usage:</h3><p>Blood thinners status unknown.</p>
+      `;
+      }
+
+      const breakdownSections = [
+        { label: "Facial Droop", class: ".FacialTable" },
+        { label: "Arm Weakness", class: ".ArmTable" },
+        {
+          label: "Speech Disturbance <i>Expressive</i>",
+          class: ".SpeechTable",
+        },
+        { label: "Speech Disturbance <i>Receptive</i>", class: ".FingerTable" },
+        { label: "Eye Deviation", class: ".EyeTable" },
+        { label: "Denial", class: ".DenialTable" },
+        { label: "Neglect", class: ".NeglectTable" },
+      ];
+
+      reportHtml += `<h3>FAST-ED Score Breakdown:</h3><p>`;
+      breakdownSections.forEach((section) => {
+        const val =
+          $(`${section.class} .score-cell.selected`).data("point") || 0;
+        reportHtml += `${section.label}: ${val}<br>`;
+      });
+      reportHtml += `<br>Total Score: ${scoreTotal}</p>`;
     }
+
     $(".TablesWrapper").fadeOut(300);
     $(".FastEDHeader").fadeOut(300);
+    $(".ScoreWrapper").fadeIn(200);
     $("#Report").html(reportHtml).fadeIn(200);
+
+    localStorage.setItem("fastedReport", reportHtml);
+    localStorage.setItem("fastedSubmitted", "true");
   });
 });
