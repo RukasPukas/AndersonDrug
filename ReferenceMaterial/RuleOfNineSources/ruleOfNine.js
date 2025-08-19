86729619211afa4c3cfdbbb8a5dd51737e6ee888
Lukibear <lukastyon@gmail.com>
2025-08-18 19:51:17 -0500

    Update final output message

--- ReferenceMaterial/RuleOfNineSources/ruleOfNine.js
+++ ReferenceMaterial/RuleOfNineSources/ruleOfNine.js
@@ -101,7 +101,9 @@ $(".CalculateButton").on("click", function () {
 
 
   $("#ParklandResult").html(
+
     `
+    With ${tbsaPct}% estimated body surface area burned:<br><br>
      Over the first 8 hours: <strong>${Math.round(first8)} mL</strong> (${Math.round(rate8)} mL/hr)<br><br>
      Over the next 16 hours: <strong>${Math.round(next16)} mL</strong> (${Math.round(rate16)} mL/hr)<br><br>
      Over total 24 hours: <strong>${Math.round(total24)} mL</strong><br>`
